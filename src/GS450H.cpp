45ba121730dbec54273f23aa064f7273fdb00462
Sean Murphy <murphy@halfast.com>
2022-01-25 22:43:55 -0800

    inconsistent gear

--- src/GS450H.cpp
+++ src/GS450H.cpp
@@ -312,11 +312,11 @@ void GS450HClass::Task1Ms()
    case 9:
       // -3500 (reverse) to 3500 (forward)
       if(gear==0) mg2_torque=0;//Neutral
-      if(gear==32) mg2_torque=this->scaledTorqueTarget;//Drive
-      if(gear==-32) mg2_torque=this->scaledTorqueTarget*-1;//Reverse
+      if(gear==1) mg2_torque=this->scaledTorqueTarget;//Drive
+      if(gear==-1) mg2_torque=this->scaledTorqueTarget*-1;//Reverse
 
       mg1_torque=((mg2_torque*5)/4);
-      if(gear==-32) mg1_torque=0; //no mg1 torque in reverse.
+      if(gear==-1) mg1_torque=0; //no mg1 torque in reverse.
       Param::SetInt(Param::torque,mg2_torque);//post processed final torue value sent to inv to web interface
 
       //speed feedback
@@ -342,7 +342,7 @@ void GS450HClass::Task1Ms()
       htm_data[30]=(mg2_torque) & 0xFF; //positive is forward
       htm_data[31]=((mg2_torque)>>8) & 0xFF;
 
-      if(gear==32)
+      if(gear==1)
       {
          //forward direction these bytes should match
          htm_data[26]=htm_data[30];
@@ -351,7 +351,7 @@ void GS450HClass::Task1Ms()
          htm_data[29]=((mg2_torque/2)>>8) & 0xFF;
       }
 
-      if(gear==-32)
+      if(gear==-1)
       {
          //reverse direction these bytes should match
          htm_data[28]=htm_data[30];
