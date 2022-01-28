fc8a8ba6b1bdf1a795cc9411af7a080a541dd175
Sean Murphy <murphy@halfast.com>
2022-01-27 20:39:16 -0800

    enable mg2 torque for gs450h

--- src/GS450H.cpp
+++ src/GS450H.cpp
@@ -197,9 +197,9 @@ void GS450HClass::Task1Ms()
       break;
    case 4:
       // -3500 (reverse) to 3500 (forward)
-      //if(gear==0) mg2_torque=0;//Neutral
-      //if(gear==32) mg2_torque=this->scaledTorqueTarget;//Drive
-      //if(gear==-32) mg2_torque=this->scaledTorqueTarget*-1;//Reverse
+      if(gear==0) mg2_torque=0;//Neutral
+      if(gear==32) mg2_torque=this->scaledTorqueTarget;//Drive
+      if(gear==-32) mg2_torque=this->scaledTorqueTarget*-1;//Reverse
 
       mg1_torque=((mg2_torque*5)/4);
       if (scaledTorqueTarget < 0) mg1_torque=0; //no mg1 torque in reverse.
@@ -312,8 +312,8 @@ void GS450HClass::Task1Ms()
    case 9:
       // -3500 (reverse) to 3500 (forward)
       if(gear==0) mg2_torque=0;//Neutral
-      if(gear==1) mg2_torque=this->scaledTorqueTarget;//Drive
-      if(gear==-1) mg2_torque=this->scaledTorqueTarget*-1;//Reverse
+      if(gear==32) mg2_torque=this->scaledTorqueTarget;//Drive
+      if(gear==-32) mg2_torque=this->scaledTorqueTarget*-1;//Reverse
 
       mg1_torque=((mg2_torque*5)/4);
       if(gear==-1) mg1_torque=0; //no mg1 torque in reverse.
