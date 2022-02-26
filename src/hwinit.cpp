70d2d22ac883365a896097d3a2f56c7dd9826ab2
Sean Murphy <murphy@halfast.com>
2022-02-25 22:26:38 -0800

    Working web on stm32_p107

--- src/hwinit.cpp
+++ src/hwinit.cpp
@@ -130,8 +130,8 @@ HWREV detect_hw()
 */
 void usart_setup(void)
 {
-    gpio_set_mode(TERM_USART_TXPORT, GPIO_MODE_OUTPUT_50_MHZ,
-                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, TERM_USART_TXPIN);
+    gpio_set_mode(GPIO_BANK_USART3_FR_TX, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART3_FR_TX);
     usart_set_baudrate(TERM_USART, USART_BAUDRATE);
     usart_set_databits(TERM_USART, 8);
     usart_set_stopbits(TERM_USART, USART_STOPBITS_2);
